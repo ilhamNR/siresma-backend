bfb3ecd424f9917a8940c038047930a05355fea2
Ilham Nur Ramadhan <ilhamnurramadhan19@gmail.com>
2023-06-25 11:22:36 +0700

    add database migration

---
+++ database/migrations/2023_06_25_041353_create_trash_store_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trash_store_logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->date('store_date');
+            $table->string('trash_category');
+            $table->string('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trash_store_logs');
+    }
+};
