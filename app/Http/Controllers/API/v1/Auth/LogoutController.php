84358863743d1dd1164a721835880df502740243
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-07-02 22:30:07 +0700

    add logout controller

---
+++ app/Http/Controllers/API/v1/Auth/LogoutController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Traits\APIResponseTrait;
+
+class LogoutController extends Controller
+{
+    use APIResponseTrait;
+
+    public function logout()
+    {
+        try {
+            Auth::user()->currentAccessToken()->delete();
+            return $this->success("Success", null, 200);
+        } catch (\Exception $e) {
+            return $this->error("Failed", 401);
+        }
+    }
+}
