f12cc9683ede0409365efc90e34c63a96033b906
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-07-02 22:28:51 +0700

    add chooseBankSampah function

--- app/Http/Controllers/API/v1/TrashManagement/TrashBankController.php
+++ app/Http/Controllers/API/v1/TrashManagement/TrashBankController.php
@@ -6,13 +6,34 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\TrashBank;
 use App\Traits\APIResponseTrait;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class TrashBankController extends Controller
 {
     use APIResponseTrait;
-    
-    public function getBankSampah(){
-        $trashBank = TrashBank::all();
-        return $this->success("Success",$trashBank, 200);
+
+    public function getBankSampah()
+    {
+        try {
+            $trashBank = TrashBank::all();
+            return $this->success("Success", $trashBank, 200);
+        } catch (\Exception $e) {
+            return $this->error("Failed", 401);
+        }
+    }
+
+    public function chooseBankSampah(Request $request)
+    {
+        try{
+        $user = User::findOrFail(Auth::user()->id);
+        // dd($user);
+        $user->update([
+            'trash_bank_id' => $request->trash_bank_id
+        ]);
+        return $this->success("Success", null, 200);
+    } catch(\Exception $e) {
+        return $this->error("Failed", 401);
+    }
     }
 }
