9168d5a44ec9075dec42c5423bc1fc78b33912d2
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-07-02 22:28:28 +0700

    move location method from request to user entity

--- app/Http/Controllers/API/v1/Home/HomeController.php
+++ app/Http/Controllers/API/v1/Home/HomeController.php
@@ -6,17 +6,19 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Traits\APIResponseTrait;
 use App\Models\TrashBank;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     use APIResponseTrait;
 
-    public function index(Request $request)
+    public function index()
     {
-        // $location = $request->location;
-        $location = TrashBank::select('id', 'name', 'description')->findorFail($request->location);
-
-        // $trashSummary =
-        return $this->success("success", $location, 200);
+        try {
+            $location = TrashBank::select('id', 'name', 'description')->findorFail(Auth::user()->trash_bank_id);
+            return $this->success("success", $location, 200);
+        } catch (\Exception $e) {
+            return $this->error("Failed", 401);
+        }
     }
 }
