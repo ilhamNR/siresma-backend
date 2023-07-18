a045d24568978449706fa4a63f0a2d14ea7c2349
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-07-18 19:38:18 +0700

    update trashcontroller

--- app/Http/Controllers/API/v1/TrashManagement/TrashController.php
+++ app/Http/Controllers/API/v1/TrashManagement/TrashController.php
@@ -18,21 +18,33 @@ class TrashController extends Controller
     public function list()
     {
         $user = Auth::user()->id;
-        $data = GarbageSavingsData::where('user_id', $user)->get();
+        $data = GarbageSavingsData::where('user_id', $user)->with('iot')->get();
+        $data = $data->map(function ($item) {
+            // hide iot id
+            unset($item->iot_id);
+
+            return $item;
+        });
         return $this->success('Success', $data, 200);
+    }
+
+    public function generateCode()
+    {
+        do {
+            $code = Str::random(7);
+            $generatedCode = Str::upper($code);
+            $existingCode = IOT::where('code', $generatedCode)->first();
+        } while (isset($existingCode));
+        return $generatedCode;
     }
     public function storeTrash(Request $request)
     {
-        $code = Str::random(7);
-        $generatedCode = Str::upper($code);
-
         try {
             DB::beginTransaction();
             GarbageSavingsData::create([
                 'user_id' => Auth::user()->id,
                 'balance' => 10000,
                 'trash_category' => $request->trash_category,
-                'generated_code' => $generatedCode,
                 'store_date' => $request->store_date
             ]);
             DB::commit();
@@ -43,24 +55,47 @@ class TrashController extends Controller
         }
     }
 
-    public function connectIOT(Request $request)
+    public function storeIOT(Request $request)
     {
-        $trashStore = GarbageSavingsData::where('generated_code', $request->code)->first();
-
         try {
-            IOT::create([
+            DB::beginTransaction();
+            $iot = IOT::create([
                 'weight' => $request->weight,
-                'garbage_savings_data_id' => $trashStore->id
-            ]);
-
-            $trashStore->update([
-                'weight' => $request->weight
+                'code' => TrashController::generateCode()
             ]);
             DB::commit();
-            return $this->success($trashStore, 200);
+            return $this->success($iot->code, 200);
         } catch (\Exception $e) {
             DB::rollBack();
             return $this->error("Failed", 401);
+        }
+    }
+
+    public function connectIOT(Request $request)
+    {
+        $iot_data = IOT::where('code', $request->code)->first();
+        $garbage_savings_data = GarbageSavingsData::where('id', $request->garbage_savings_data_id)->first();
+        $redeemed_iot = GarbageSavingsData::where('iot_id', $iot_data->id)->first();
+        if (is_null($iot_data)) {
+            return $this->error("Kode tidak valid", 401);
+        } else if (isset($redeemed_iot)) {
+            return $this->error("IOT sudah dihubungkan ke data sampah lain", 401);
+        } else if (is_null($garbage_savings_data)) {
+            return $this->error("Data stor sampah tidak valid", 401);
+        } else if ($garbage_savings_data->user_id != Auth::user()->id) {
+            return $this->error("Data Sampah ini bukan milik anda", 401);
+        } else {
+
+            try {
+                DB::beginTransaction();
+                $garbage_savings_data->update([
+                    'iot_id' =>  $iot_data->id
+                ]);
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollBack();
+                return $this->error("Failed", 401);
+            }
         }
     }
 
