816270a4a8e6b42e77226e31a8109fa249daa2cc
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-07-19 22:28:37 +0700

    update migration

--- database/migrations/2023_07_19_060607_add_trash_category_id_foreign_keys_to_garbage_savings_datas_table.php
+++ database/migrations/2023_07_19_060607_add_trash_category_id_foreign_keys_to_garbage_savings_datas_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
     public function up()
     {
         Schema::table('garbage_savings_datas', function (Blueprint $table) {
-            $table->unsignedBigInteger('trash_category_id')->index('garbage_savings_datas_trash_category_foreign')->after('user_id');
+            // // use this if there's already inserted data on garbage savings table
+            // $table->unsignedBigInteger('trash_category_id')->index('garbage_savings_datas_trash_category_id_foreign')->after('user_id')->nullable();
+
+            $table->unsignedBigInteger('trash_category_id')->index('garbage_savings_datas_trash_category_id_foreign')->after('user_id')->nullable();
+
             $table->foreign(['trash_category_id'])->references(['id'])->on('trash_categories');
         });
     }
