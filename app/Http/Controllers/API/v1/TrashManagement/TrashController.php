f12e341f6790a841d82223ca267aed18faf47b1b
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-07-22 12:47:01 +0700

    add controller to calculate balance admin and user

--- app/Http/Controllers/API/v1/TrashManagement/TrashController.php
+++ app/Http/Controllers/API/v1/TrashManagement/TrashController.php
@@ -117,10 +117,16 @@ class TrashController extends Controller
         } else {
 
             try {
+                // calculate price
+                $total_price = TrashController::calculatePrice($garbage_savings_data, $iot_data->weight);
+                $admin_balance = $total_price*40/100;
+                $user_balance = $total_price*60/100;
+
                 DB::beginTransaction();
                 $garbage_savings_data->update([
                     'iot_id' =>  $iot_data->id,
-                    'price' => TrashController::calculatePrice($garbage_savings_data, $iot_data->weight)
+                    'user_balance' => $user_balance,
+                    'admin_balance' => $admin_balance
                 ]);
                 DB::commit();
                 return $this->success("Data IOT sudah terhubung", 200);
