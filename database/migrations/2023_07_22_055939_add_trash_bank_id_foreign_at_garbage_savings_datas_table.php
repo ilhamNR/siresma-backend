9fda064ec0d03dbfa4d7001e8a99e61f61007630
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-07-22 13:50:50 +0700

    migration for trash bank id on garbagesavings

---
+++ database/migrations/2023_07_22_055939_add_trash_bank_id_foreign_at_garbage_savings_datas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('garbage_savings_datas', function (Blueprint $table) {
+            // // use this if there's already inserted data on garbage savings table
+            // $table->unsignedBigInteger('trash_category_id')->index('garbage_savings_datas_trash_category_id_foreign')->after('user_id')->nullable();
+
+            $table->unsignedBigInteger('trash_bank_id')->index('garbage_savings_datas_trash_bank_id_foreign')->after('trash_category_id')->nullable();
+
+            $table->foreign(['trash_bank_id'])->references(['id'])->on('trash_banks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('garbage_savings_datas', function (Blueprint $table) {
+            $table->dropForeign('garbage_savings_datas_trash_bank_id_foreign');
+            $table->dropColumn('trash_bank_id');
+        });
+    }
+};
