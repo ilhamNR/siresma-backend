2762138a220ad075768facf3e4bf1adae70bdc81
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-07-30 20:31:46 +0700

    increase photo profile limit to 5120KB

---
+++ app/Http/Controllers/API/v1/Auth/ProfileController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\API\v1\Auth;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(){
+        
+    }
+}
