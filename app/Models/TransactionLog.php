92512d9033719290fe1a06f5276e3ac570fa88a7
ilhamNR <ilhamnurramadhan19@gmail.com>
2023-08-02 19:15:25 +0700

    add relation to TransactionLog

--- app/Models/TransactionLog.php
+++ app/Models/TransactionLog.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\GarbageSavingsData;
 
 class TransactionLog extends Model
 {
     /**
      * @var array
      */
-    protected $fillable = ['code', 'type','user_id','garbage_savings_data_id' ,'amount','is_approved','created_at', 'updated_at'];
+    protected $fillable = ['code', 'type', 'user_id', 'garbage_savings_data_id', 'amount', 'trash_bank_id' , 'is_approved', 'created_at', 'updated_at'];
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
@@ -18,5 +19,10 @@ class TransactionLog extends Model
     public function users()
     {
         return $this->hasMany('App\Models\User');
+    }
+
+    public function garbageSavingsData()
+    {
+        return $this->belongsTo(GarbageSavingsData::class, 'garbage_savings_data_id');
     }
 }
